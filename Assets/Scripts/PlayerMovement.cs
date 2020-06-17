ee3d35a8462855f595d9eff4c76d92ea83290634
EpicBlade <51732350+jaydeep-dev@users.noreply.github.com>
2020-06-17 12:43:47 +0530

    Player Movement
    making player move left & right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,78 +2,44 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    #region Variables
-    //private variables
     Rigidbody2D rb = null;
-    Vector2 dir = Vector2.zero;
     Animator animator = null;
     bool isGrounded = false;
 
-    //serialized variable
     [SerializeField] float speed = 5f;
-    [SerializeField] float jumpPower = 0f;
-    [SerializeField] float checkRadius = 0f;
-    [SerializeField] LayerMask groundLayer = 0;
+    [SerializeField] float jumpPower = 10f;
     [SerializeField] Transform groundCheck = null;
-    [SerializeField] Transform particalaTransform = null;
-    [SerializeField] ParticleSystem dirtParticles = null;
-    #endregion
-    private void Start()
+    [SerializeField] float checkRadius = 0.4f;
+    [SerializeField] LayerMask groundLayer = 0;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //getting input from Player
-        dir = new Vector2
-        {
-            x = Input.GetAxis("Horizontal") * speed * Time.deltaTime,
-            y = 0f
-        }.normalized;
-
-        //checking for player to be on ground
+        float horizontal = Input.GetAxis("Horizontal");
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
 
-        //setting the animation
-        animator.SetFloat("Speed", Mathf.Abs(dir.x));
         animator.SetBool("IsGrounded", isGrounded);
         animator.SetFloat("Velocity", rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
+        if (horizontal > 0)
+            transform.GetComponent<SpriteRenderer>().flipX = false;
+        else if (horizontal < 0)
+            transform.GetComponent<SpriteRenderer>().flipX = true;
 
-        //fliping the player
-        if (dir.x < 0)
-        {
-            transform.gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            particalaTransform.localScale = -Vector2.one;
-        }
-        else if (dir.x > 0)
-        {
-            transform.gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            particalaTransform.localScale = Vector2.one;
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        //making player jump
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
-            rb.velocity = Vector2.up * jumpPower;
+            rb.AddForce(Vector2.up * jumpPower,ForceMode2D.Impulse);
+            Debug.Log("Jumped");
         }
 
-        if (Mathf.Abs(dir.x) > 0 && isGrounded)
-        {
-            dirtParticles.Play();
-        }
-        else
-        {
-            dirtParticles.Stop();
-        }
-
-        //making player move
-        rb.MovePosition(dir);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 }
